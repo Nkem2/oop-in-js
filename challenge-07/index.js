0985803a852413b00222dd442b46224c114a6d94
chinelo <chinelookeke015@gmail.com>
2023-02-09 15:38:12 +0100

    challenge-07 done

---
+++ challenge-07/index.js
@@ -0,0 +1,11 @@
+const classroom= {
+  hasTeachingAssistant: true,
+  classList:["rashida","james","lisa","omair","lukas"]
+
+}
+function getStudents(classroom){
+  let teacher=classroom[0]
+if(hasTeachingAssistant= True){
+
+}
+}
