3770be249bae423ed1e4e176b0b387b400ca2742
chinelo <chinelookeke015@gmail.com>
2023-02-09 14:06:21 +0100

    challenge-01 done

---
+++ challenge-01/index.js
@@ -0,0 +1,13 @@
+ Book = class book{
+  constructor(Title,Author,ISBN,NumberofCopies,availability,CopiesSold){
+    this.Title=Title;
+    this.Author=Author;
+    this.ISBN=ISBN;
+    this.NumberofCopies
+    this.availability=()=>{
+      if(NumberofCopies===0)
+      return "out of Stock"
+    }
+    this.CopiesSold=
+  }
+ }
